7f4c345582e48dc3765ef8fc9835c727b5284e23
Nick Sikkema <contact@nicksikkema.com>
2026-06-13 14:15:39 -0500

    build: add basic crate setup

---
+++ crates/basic/src/lib.rs
@@ -0,0 +1,17 @@
+//! A basic crate for demonstrating an addition function.
+
+/// Adds two unsigned 64-bit integers.
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
